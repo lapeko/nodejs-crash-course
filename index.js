1fa97cb10ca45739842bb118fc11bf430d023b24
Vitali Lapeka <lapeko88@gmail.com>
2023-01-12 23:51:14 +0100

    add http server with several routes

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-const Logger = require('./logger');
+const http = require('http');
+const fs = require('fs/promises');
+const path = require('path');
 
-const logger = new Logger();
+const PORT = process.env.PORT || 8000;
 
-logger.on('log', console.log);
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/") {
+    const file = await fs.readFile('./public/home.html');
+    res.writeHead(200, "Ok", { "Content-type": "text/html" });
+    return res.end(file);
+  }
 
-logger.log('Hello');
-logger.log('Hi');
-logger.log('Qwerty');
+  if (req.url === "/about") {
+    const file = await fs.readFile('./public/about.html');
+    res.writeHead(200, "Ok", { "Content-type": "text/html" });
+    return res.end(file);
+  }
+
+  if (req.url === "/users") {
+    const users = [
+      { name: "Bob Pupkins", age: 25 },
+      { name: "Stiw Qwerty", age: 47 }
+    ];
+    res.writeHead(200, "Ok", { "Content-type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
